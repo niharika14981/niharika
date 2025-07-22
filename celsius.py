60215de5d940b35f0f8cdd000510f859d37c58f9
niharika14981 <niharikakalamadugu@gmail.com>
2025-07-22 16:25:07 +0530

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,7 @@
+#celsius to fahrenheit
+celsius=float(input())
+fahrenheit=float(input())
+fahrenheit=celsius*9/5+32
+celsius=fahrenheit-32*5/9
+print(str(fahrenheit)+'F')
+print(str(celsius)+'c')
---
+++ quradic eqn.py
@@ -0,0 +1,8 @@
+a=3
+b=-5
+c=2
+val=((b**2)-(4*a*c))**0.5
+deno=2*a
+root1=(-b+val)/deno
+root2=(-b-val)/deno
+print(root1,root2)
